9af2249c43b92b03b25ec0cc7d0cb550b0448eb6
kakalotto <ewu@cpsd.us>
2024-06-19 14:03:24 -0400

    Finish the work on programs pages

--- js/swiper.js
+++ js/swiper.js
@@ -8,20 +8,20 @@ const swiper = new Swiper('.swiper', {
 
 
   breakpoints: {
-      800: {
-          slidesPerView: 2,
-      },
-      1100: {
-          slidesPerView: 3,
-      }
+    800: {
+      slidesPerView: 2,
+    },
+    1100: {
+      slidesPerView: 3,
+    }
   },
   loop: true,
   autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-      reverseDirection: true,
+    delay: 7500,
+    disableOnInteraction: false,
+    reverseDirection: true,
 
-    },
+  },
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -36,5 +36,35 @@ const swiper = new Swiper('.swiper', {
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
+  },
+});
+
+var swiper2 = new Swiper(".swiperPrograms", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  autoplay: {
+    delay: 500000000000000000000000000000000000,
+  },
+  loop: false,
+  watchOverflow: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  grid: {
+    rows: 2,
+  },
+  
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
